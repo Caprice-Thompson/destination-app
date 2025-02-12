2aa69387f65888a026dbc4d853bdd65c72ee7da8
Caprice-Thompson <caprice.thompson@itv.com>
2025-02-12 21:52:08 +0000

    ..

--- frontend/src/config/endpoints.config.ts
+++ frontend/src/config/endpoints.config.ts
@@ -12,25 +12,25 @@ interface EndpointConfig {
 
 const endpoints: EndpointConfig = {
     prod: {
-        country: import.meta.env.VITE_COUNTRY_API_URL || 'h',
-        tourism: import.meta.env.VITE_TOURISM_API_URL || '',
-        earthquake: import.meta.env.VITE_EARTHQUAKE_API_URL || '',
-        volcano: import.meta.env.VITE_VOLCANO_API_URL || ''
+        country: import.meta.env.VITE_COUNTRY_API_URL,
+        tourism: import.meta.env.VITE_TOURISM_API_URL,
+        earthquake: import.meta.env.VITE_EARTHQUAKE_API_URL,
+        volcano: import.meta.env.VITE_VOLCANO_API_URL
     },
     dev: {
-        country: import.meta.env.VITE_LOCAL_API_URL || '',
-        tourism: import.meta.env.VITE_LOCAL_API_URL || '',
-        earthquake: import.meta.env.VITE_LOCAL_API_URL || '',
-        volcano: import.meta.env.VITE_LOCAL_API_URL || ''
+        country: import.meta.env.VITE_LOCAL_API_URL,
+        tourism: import.meta.env.VITE_LOCAL_API_URL,
+        earthquake: import.meta.env.VITE_LOCAL_API_URL,
+        volcano: import.meta.env.VITE_LOCAL_API_URL
     }
 };
 
 export const getEndpoints = (): ServiceEndpoints => {
     if (!import.meta.env.VITE_COUNTRY_API_URL) {
         console.warn('Environment variables not loaded properly');
-        console.log(import.meta.env.VITE_COUNTRY_API_URL);
-        console.log(import.meta.env.PROD);
-        console.log(endpoints.prod);
+        console.warn(import.meta.env.VITE_COUNTRY_API_URL);
+        console.warn(import.meta.env.PROD);
+        console.warn(endpoints.prod);
     }
     return import.meta.env.PROD ? endpoints.prod : endpoints.dev;
 }; 
