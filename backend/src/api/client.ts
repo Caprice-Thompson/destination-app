dbe4b571c806b4631104502eb5149fc1aa22dc71
Caprice-Thompson <caprice.thompson@itv.com>
2025-02-14 23:09:25 +0000

    up

--- backend/src/api/client.ts
+++ backend/src/api/client.ts
@@ -7,8 +7,8 @@ type RequestOptions = {
 };
 
 const headers: Record<string, string> = {
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET",
+  "Access-Control-Allow-Headers": "Content-Type, Apigw-Requestid",
   "Access-Control-Allow-Origin": process.env.FRONTEND_URL || "*",
 };
 
