85e2d8a2d6247720ebc870a3f48f61974d9947ac
Caprice-Thompson <caprice.thompson@itv.com>
2025-05-02 17:09:58 +0100

    update support file

--- tests/cypress/support/e2e.js
+++ tests/cypress/support/e2e.js
@@ -3,7 +3,6 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 });
 // Your code can be before this line
 // require('./commands')
-require('dd-trace/ci/cypress/support');
 // Also supported:
 // import 'dd-trace/ci/cypress/support'
 // Your code can also be after this line
